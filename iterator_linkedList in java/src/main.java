885bcf8fb3100e6a71951c091c35d0786a1fb8d4
umesh <69199558+494494@users.noreply.github.com>
2026-01-12 12:43:30 +0530

    Implement list iteration with user input
    Added an iterator to navigate through the list of places.

--- iterator_linkedList in java/src/main.java
+++ iterator_linkedList in java/src/main.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.Scanner;
 
 record Places(String name, int distance){
     @Override
@@ -17,6 +18,51 @@ public class main {
         addPlace(list,new Places("sambhajinagar",300));
         addPlace(list,new Places("Pune",418));
         System.out.println(list);
+
+        var iterator= list.listIterator(); //iterator
+        Scanner scanner =new Scanner(System.in);
+        boolean quitloop=false;
+        boolean forward=true;
+
+
+        printMenu(); //printing menu
+        while(!quitloop){
+
+            if(!iterator.hasPrevious()){
+                System.out.println("Start of List " + iterator.next());
+                forward=true;
+            }
+            if(!iterator.hasNext()){
+                System.out.println("final "+ iterator.previous());
+                forward=false;
+            }
+
+            System.out.print("Enter the Value: ");
+            String s= scanner.nextLine().substring(0,1).toUpperCase();
+            switch (s){
+                case "F":
+                    System.out.println("user want to go forward");
+                    if(iterator.hasNext()){
+                        System.out.println(iterator.next());
+                    }
+                    break;
+                case "B":
+                    System.out.println("user want to go backward");
+                    if(iterator.hasPrevious()){
+                        System.out.println(iterator.previous());
+                    }
+                    break;
+                case "L":
+                    System.out.println(list);
+                    break;
+                case "M":
+                    printMenu();
+                    break;
+                default:
+                   quitloop=true;
+                    break;
+            }
+        }
 
     }
     public static void addPlace(LinkedList<Places> list,Places place){
@@ -41,4 +87,14 @@ public class main {
         }
         list.add(place);
     }
+    private static void printMenu(){
+        System.out.println("""
+                Available actions (select word or letter):
+                (F)orward
+                (B)ackward
+                (L)ist Places
+                (M)enu
+                Q)uit""");
+    }
+
 }
